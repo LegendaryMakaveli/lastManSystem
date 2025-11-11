db6a4c544883dbd887061691d48fd4cc3177bf3d
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 19:03:42 +0100

    Officer mapper

--- src/utils/OfficerMapper.java
+++ src/utils/OfficerMapper.java
@@ -1,4 +1,13 @@
 package utils;
 
+import data.models.Officer;
+import dtos.requests.RegisterOfficerRequest;
+
 public class OfficerMapper {
+
+    public static Officer map(RegisterOfficerRequest request) {
+        Officer newOfficer = new Officer();
+        newOfficer.setName(request.getName());
+        return newOfficer;
+    }
 }
