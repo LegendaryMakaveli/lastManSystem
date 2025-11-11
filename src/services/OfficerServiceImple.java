d04ea18befc320a71c816c4b39349367a22d85df
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 19:00:33 +0100

    officer service concrete class

--- src/services/OfficerServiceImple.java
+++ src/services/OfficerServiceImple.java
@@ -1,4 +1,22 @@
 package services;
 
-public class OfficerServiceImple {
+import data.models.Officer;
+import data.repositories.OfficerRepositories;
+import data.repositories.Officers;
+import dtos.requests.RegisterOfficerRequest;
+import dtos.responses.RegisterOfficerResponse;
+
+import static utils.OfficerMapper.map;
+
+public class OfficerServiceImple implements  OfficerService {
+    private OfficerRepositories officers = new Officers();
+
+
+    @Override
+    public RegisterOfficerResponse registerOfficer(RegisterOfficerRequest request) {
+        Officer newOfficer = map(request);
+        officers.save(newOfficer);
+
+        return new  RegisterOfficerResponse();
+    }
 }
