2a71790e9ab38e0fa019846591259b73985648fe
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 18:59:52 +0100

    officer service interface

--- src/services/OfficerService.java
+++ src/services/OfficerService.java
@@ -1,4 +1,8 @@
 package services;
 
+import dtos.requests.RegisterOfficerRequest;
+import dtos.responses.RegisterOfficerResponse;
+
 public interface OfficerService {
+    RegisterOfficerResponse registerOfficer(RegisterOfficerRequest request);
 }
