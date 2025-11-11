5a43fdffabe62080e464aa1068d724210d82ce70
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 19:02:11 +0100

    vehicle service concrete class

--- src/services/VehicleServiceImple.java
+++ src/services/VehicleServiceImple.java
@@ -4,15 +4,17 @@ import data.repositories.VehicleRepositories;
 import data.repositories.Vehicles;
 import dtos.requests.RegisterVehicleRequest;
 import dtos.responses.RegisterVehicleResponse;
+import exceptions.VehicleNotFoundException;
 
 import static utils.Mapper.map;
 
-public class VehicleSericeImple implements VehicleService{
+public class VehicleServiceImple implements VehicleService{
     private VehicleRepositories vehicles = new Vehicles();
 
 
     @Override
     public RegisterVehicleResponse registerVehicle(RegisterVehicleRequest request) {
+        if(vehicles.findByChasisNumber(request.getVehicleChasisNumber()) != null) throw new VehicleNotFoundException("Vehicle already exists");
         Vehicle vehicle = map(request);
         vehicles.save(vehicle);
 
