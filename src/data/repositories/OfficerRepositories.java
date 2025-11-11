52d8a0255185cf920c411d13d6fd114f9a72aa37
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 18:46:17 +0100

    update on officer repo

--- src/data/repositories/OfficerRepositories.java
+++ src/data/repositories/OfficerRepositories.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface OfficerRepositories {
     Officer save(Officer officer);
-    Officer findById(int id);
+    Officer findById(String id);
     List<Officer> findAll();
-    void deleteById(int id);
+    void deleteById(String id);
     int getSize();
     void deleteAll();
     Officer delete(Officer officer);
