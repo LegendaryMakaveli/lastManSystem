e1e1dbdec33f1fa99d4f27d59e8072bca68944a3
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 19:01:23 +0100

    vehicle service interface

--- src/services/VehicleService.java
+++ src/services/VehicleService.java
@@ -1,4 +1,8 @@
 package services;
 
+import dtos.requests.RegisterVehicleRequest;
+import dtos.responses.RegisterVehicleResponse;
+
 public interface VehicleService {
+    RegisterVehicleResponse registerVehicle(RegisterVehicleRequest request);
 }
