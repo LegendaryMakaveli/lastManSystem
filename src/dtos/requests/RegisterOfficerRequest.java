eaef5b0f4656374e8ed54454b13d347f00c97886
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-11 18:53:26 +0100

    request on registering an officer

--- src/dtos/requests/RegisterOfficerRequest.java
+++ src/dtos/requests/RegisterOfficerRequest.java
@@ -1,4 +1,13 @@
 package dtos.requests;
 
 public class RegisterOfficerRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
