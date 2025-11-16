b51183bec795871da73904050c1ec8fe74d4f387
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-16 04:10:37 +0100

    update Ticket Service

--- src/services/TicketsService.java
+++ src/services/TicketsService.java
@@ -5,4 +5,6 @@ import dtos.responses.BookTicketResponse;
 
 public interface TicketsService {
     BookTicketResponse bookTicket(BookTicketRequest request);
+    BookTicketResponse viewBookedTicket(BookTicketRequest request);
+    BookTicketResponse settleTicket(BookTicketRequest request);
 }
