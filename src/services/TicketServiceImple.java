12081a08c205931b340e2423ca3054e89b08a271
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-16 04:09:58 +0100

    update Ticket Service implementation

--- src/services/TicketServiceImple.java
+++ src/services/TicketServiceImple.java
@@ -3,11 +3,10 @@ package services;
 import data.models.Officer;
 import data.models.Ticket;
 import data.repositories.OfficerRepositories;
-import data.repositories.Officers;
 import data.repositories.TicketRepositories;
-import data.repositories.Tickets;
 import dtos.requests.BookTicketRequest;
 import dtos.responses.BookTicketResponse;
+import exceptions.IdNotFoundException;
 import exceptions.LastManException;
 import static utils.OfficerMapper.mapTicket;
 import data.models.Offense;
@@ -15,16 +14,23 @@ import data.models.Offense;
 
 
 public class TicketServiceImple implements TicketsService{
-    private OfficerRepositories officers = new Officers();
-    private TicketRepositories tickets = new Tickets();
+    private OfficerRepositories officers;
+    private TicketRepositories tickets;
     private Ticket  ticket = new Ticket();
     private Offense offense;
+
+    public TicketServiceImple(OfficerRepositories officers, TicketRepositories tickets) {
+        this.officers = officers;
+        this.tickets = tickets;
+    }
+
 
     @Override
     public BookTicketResponse bookTicket(BookTicketRequest request) {
         Officer officer = officers.findById(request.getIssuerOfficerId());
-        ticket.setOfficer(officer);
         if (officer == null) throw new LastManException("Officer does not exist");
+        ticket.setOfficer(officer);
+
 
         Ticket newTicket = mapTicket(request);
         newTicket.setOffense(offense);
@@ -32,4 +38,33 @@ public class TicketServiceImple implements TicketsService{
 
         return new BookTicketResponse();
     }
+
+    @Override
+    public BookTicketResponse viewBookedTicket(BookTicketRequest request) {
+        Officer officer = officers.findById(request.getIssuerOfficerId());
+        if (officer == null) throw new LastManException("Officer does not exist");
+
+
+
+        Ticket newTicket = mapTicket(request);
+        tickets.findById(newTicket.getId());
+        if (ticket == null) throw new IdNotFoundException("Ticket not found");
+        if (!ticket.getOfficer().getId().equals(officer.getId())) throw new LastManException("You are not allowed to view this ticket");
+
+        return new  BookTicketResponse();
+    }
+
+    @Override
+    public BookTicketResponse settleTicket(BookTicketRequest request) {
+        Officer officer = officers.findById(request.getIssuerOfficerId());
+        if (officer == null) throw new LastManException("Officer does not exist");
+
+
+        Ticket newTicket = mapTicket(request);
+        if(newTicket.getHasPaid()) return new  BookTicketResponse();
+
+        throw new IdNotFoundException("Ticket not found");
+    }
+
 }
+
