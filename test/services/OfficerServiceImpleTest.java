282f60b4257a88278e5eed3ea6190b376460edfe
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-11-16 04:13:17 +0100

    update Officer Service Test

--- test/services/OfficerServiceImpleTest.java
+++ test/services/OfficerServiceImpleTest.java
@@ -7,8 +7,11 @@ import data.repositories.TicketRepositories;
 import data.repositories.Tickets;
 import dtos.requests.BookTicketRequest;
 import dtos.requests.RegisterOfficerRequest;
+import dtos.responses.BookTicketResponse;
+import exceptions.LastManException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.Year;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,14 +23,14 @@ class OfficerServiceImpleTest {
 
     @BeforeEach
     void setUp() {
-        officerService = new OfficerServiceImple();
-        ticketsService = new TicketServiceImple();
         officers = new Officers();
         tickets = new Tickets();
 
-
         officers.deleteAll();
         tickets.deleteAll();
+
+        officerService = new OfficerServiceImple(officers);
+        ticketsService = new TicketServiceImple(officers, tickets);
     }
 
     @Test
@@ -57,17 +60,99 @@ class OfficerServiceImpleTest {
         RegisterOfficerRequest newOfficer = new RegisterOfficerRequest();
         officerService.registerOfficer(newOfficer);
 
-        BookTicketRequest giveTicket = new BookTicketRequest();
-        giveTicket.setIssuerOfficerId(newOfficer.getId());
-        giveTicket.setOffenderVehicleName(giveTicket.getOffenderVehicleName());
-        giveTicket.setOffenderVehicleModel(giveTicket.getOffenderVehicleModel());
-        giveTicket.setOffenderVehicleyear(giveTicket.getOffenderVehicleyear());
-        giveTicket.setOffenderVehicleChasisNumber(giveTicket.getOffenderVehicleChasisNumber());
-        giveTicket.setOffense(Offense.OVER_SPEEDING);
-
+        BookTicketRequest giveTicket = getBookTicketRequest(newOfficer);
         ticketsService.bookTicket(giveTicket);
 
         assertEquals(1, tickets.getSize());
     }
+
+    @Test
+    public void testOfficerCanViewOwnTicket() {
+        RegisterOfficerRequest newOfficer = new RegisterOfficerRequest();
+        officerService.registerOfficer(newOfficer);
+        BookTicketRequest giveTicket = getBookTicketRequest(newOfficer);
+        ticketsService.bookTicket(giveTicket);
+
+        BookTicketRequest viewRequest = new BookTicketRequest();
+        viewRequest.setIssuerOfficerId(newOfficer.getId());
+        viewRequest.setTicketId(giveTicket.getTicketId());
+
+        BookTicketResponse newOfficerView = ticketsService.viewBookedTicket(viewRequest);
+        assertNotNull(newOfficerView);
+    }
+
+    @Test
+    public void testOfficerCannotViewAnotherOfficersTicket() {
+        RegisterOfficerRequest newOfficer = new RegisterOfficerRequest();
+        officerService.registerOfficer(newOfficer);
+        RegisterOfficerRequest newOfficerTwo = new RegisterOfficerRequest();
+        officerService.registerOfficer(newOfficerTwo);
+
+        BookTicketRequest giveTicket = getBookTicketRequest(newOfficer);
+        ticketsService.bookTicket(giveTicket);
+
+        BookTicketRequest viewRequest = new BookTicketRequest();
+        viewRequest.setIssuerOfficerId(newOfficerTwo.getId());
+        viewRequest.setTicketId(giveTicket.getTicketId());
+
+       assertThrows(LastManException.class, () -> ticketsService.viewBookedTicket(viewRequest));
+
+    }
+
+
+    @Test
+    public void testThatOfficerCanBeRegister_BookTicket_ViewTicket_AndBeAbleToSettleTicket(){
+        RegisterOfficerRequest newOfficer = new RegisterOfficerRequest();
+        officerService.registerOfficer(newOfficer);
+
+        BookTicketRequest giveTicket = getBookTicketRequest(newOfficer);
+        ticketsService.bookTicket(giveTicket);
+
+        BookTicketRequest settleTicketRequest = new BookTicketRequest();
+        settleTicketRequest.setTicketId(giveTicket.getTicketId());
+        settleTicketRequest.setHasPaid(true);
+        settleTicketRequest.setIssuerOfficerId(newOfficer.getId());
+
+        BookTicketResponse settleTicketResponse = ticketsService.settleTicket(settleTicketRequest);
+        assertNotNull(settleTicketResponse);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static BookTicketRequest getBookTicketRequest(RegisterOfficerRequest newOfficer) {
+        BookTicketRequest giveTicket = new BookTicketRequest();
+        giveTicket.setIssuerOfficerId(newOfficer.getId());
+        giveTicket.setOffenderVehicleName("TOYOTA");
+        giveTicket.setOffenderVehicleModel("CAMRY");
+        giveTicket.setOffenderVehicleyear(Year.of(2015));
+        giveTicket.setOffenderVehicleChasisNumber("SK2345CHI99FEM222EBU99");
+        giveTicket.setOffense(Offense.OVER_SPEEDING);
+
+        return giveTicket;
+    }
+
 
 }
